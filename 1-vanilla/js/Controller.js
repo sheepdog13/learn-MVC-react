6728522e80e7acd2f2f48ed38ee3a8750e9294ae
sheepdog13 <mikecho1234@naver.com>
2024-04-10 17:09:13 +0900

    검색폼 구현

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,10 +1,23 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, views) {
+  constructor(store, { searchFormView }) {
     console.log(tag);
     this.store = store;
 
-    // TODO
+    this.searchFormView = searchFormView;
+
+    this.subscribleViewEvents();
+  }
+  subscribleViewEvents() {
+    this.searchFormView
+      .on("@submit", (event) => this.search(event.detail.value))
+      .on("@reset", (event) => this.reset(event));
+  }
+  search(keyword) {
+    console.log(tag, keyword);
+  }
+  reset() {
+    console.log(tag, "reset");
   }
 }
---
+++ 1-vanilla/js/views/SearchFormView.js
@@ -0,0 +1,45 @@
+import { on, qs } from "../helpers.js";
+import View from "./View.js";
+const tag = "[view]";
+
+export default class SearchFormView extends View {
+  constructor() {
+    super(qs("#search-form-view"));
+
+    this.inputElement = qs("[type=text]", this.element);
+    this.resetElement = qs("[type=reset]", this.element);
+    this.showResetButton(false);
+
+    this.bindEvent();
+  }
+  showResetButton(visible = true) {
+    this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+    this.on("submit", (event) => this.handleSubmit(event));
+    on(this.resetElement, "click", () => this.handleReset());
+  }
+
+  handleKeyup() {
+    console.log(tag, "handlekeyup", this.inputElement.value);
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
+    if (value.length <= 0) {
+      this.handleReset();
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(tag, "submit", this.inputElement.value);
+    const { value } = this.inputElement;
+    this.emit("@submit", { value });
+  }
+
+  handleReset() {
+    console.log(tag, "reset");
+    this.emit("@reset");
+  }
+}

9942081cb260517ffdd5743d988970db615f02af
sheepdog13 <mikecho1234@naver.com>
2024-05-07 18:48:31 +0900

    SPA : label, 유효성 검사, pattern, title

--- SPA/src/components/Form.js
+++ SPA/src/components/Form.js
@@ -1,22 +1,54 @@
-export const input = (type, id, text) => {
+export const input = (type, id, text, required = false) => {
   const span = document.createElement("span");
   span.setAttribute("class", "form_elem");
+
+  const label = document.createElement("label");
+  label.setAttribute("for", id);
+  label.appendChild(document.createTextNode(text));
+
+  if (required) {
+    const markSpan = document.createElement("span");
+    markSpan.setAttribute("class", "mark");
+    markSpan.appendChild(document.createTextNode("(필수*)"));
+    label.appendChild(markSpan);
+  }
+
+  span.appendChild(label);
 
   const input = document.createElement("input");
   input.setAttribute("type", type);
   input.setAttribute("id", id);
   input.setAttribute("placeholder", text);
+  if (required) input.setAttribute("required", "");
+  if (id == "name") {
+    input.setAttribute("pattern", "^([가-힣]){2,4}$");
+    input.setAttribute("title", "2~4 글자의 한글만 입력이 가능합니다.");
+  }
   span.appendChild(input);
+
   document.getElementById("grepp_form").appendChild(span);
 };
 
-export const select = (id, optValList, optTxtList) => {
+export const select = (id, optValList, optTxtList, text, required) => {
   const span = document.createElement("span");
   span.setAttribute("class", "form_elem");
+
+  const label = document.createElement("label");
+  label.setAttribute("for", id);
+  label.appendChild(document.createTextNode(text));
+  if (required) {
+    const markSpan = document.createElement("span");
+    markSpan.setAttribute("class", "mark");
+    markSpan.appendChild(document.createTextNode("(필수*)"));
+    label.appendChild(markSpan);
+  }
+
+  span.appendChild(label);
 
   const select = document.createElement("select");
   select.setAttribute("id", id);
   select.setAttribute("name", id);
+  if (required) select.setAttribute("required", "");
   for (let i = 0; i < optValList.length; i++) {
     const option = document.createElement("option");
     option.setAttribute("value", optValList[i]);
--- SPA/src/components/SignupView.js
+++ SPA/src/components/SignupView.js
@@ -13,14 +13,55 @@ export default class SignupView {
     div.appendChild(form);
     this.$main.appendChild(div);
 
-    input("text", "name", "이름");
-    input("email", "email", "이메일");
+    input("text", "name", "이름", true);
+    input("email", "email", "이메일", true);
     input("text", "nickname", "닉네임");
+    const mbtiValList = [
+      "",
+      "ENFJ",
+      "ENTJ",
+      "ENFP",
+      "ENTP",
+      "ESFJ",
+      "ESTJ",
+      "ESFP",
+      "ESTP",
+      "INFJ",
+      "INTJ",
+      "INFP",
+      "INTP",
+      "ISFJ",
+      "ISTJ",
+      "ISFP",
+      "ISTP",
+    ];
+    const mbtiTxtList = [
+      "MBTI를 선택해주세요",
+      "ENFJ",
+      "ENTJ",
+      "ENFP",
+      "ENTP",
+      "ESFJ",
+      "ESTJ",
+      "ESFP",
+      "ESTP",
+      "INFJ",
+      "INTJ",
+      "INFP",
+      "INTP",
+      "ISFJ",
+      "ISTJ",
+      "ISFP",
+      "ISTP",
+    ];
     select(
       "role",
       ["", "backend", "frontend", "fullstack"],
-      ["직군을 선택해주세요", "백엔드", "프론트엔드", "풀스택"]
+      ["직군을 선택해주세요", "백엔드", "프론트엔드", "풀스택"],
+      "직군",
+      true
     );
+    select("mbti", mbtiValList, mbtiTxtList, "mbti");
     button("submit", "등록");
   }
 }
